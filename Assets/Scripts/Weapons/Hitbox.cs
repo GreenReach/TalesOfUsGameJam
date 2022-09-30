571fa51db22cad6e48d5404d5e1b2f9871a9bb4b
Ugiababecicu <raresgabrielalexe@yahoo.ro>
2022-10-01 02:35:58 +0300

    Player with palos

---
+++ Assets/Scripts/Weapons/Hitbox.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Enemies;
+public class Hitbox : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Collider2D collider;
+    void Start()
+    {
+        
+    }
+
+    public void hit(int dmg){
+        Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale/2, transform.rotation, LayerMask.GetMask("Default"));
+ 
+        foreach(Collider collider in colliders)
+        {
+            collider.gameObject.GetComponent<Enemy>().DoDamage(dmg);
+            Debug.Log("aaa");
+        }
+    }
+}
--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -6,88 +6,50 @@ using Player;
 public class Sword : GeneralWeapon
 {
     public GameObject target;
-    public float start =45;
-    public float finish=135;
-    public int dir = 1;
     public float timer = 0;
-    public Direction direction = Direction.Right;
     public bool first = true;
-    float angle;
     public bool stop = false;
-    public List<Direction> dire= new List<Direction>();
     void Start()
     {
-        transform.position = new Vector3(target.transform.position.x + 1.5f, target.transform.position.y, 0);
-        checkDir();
-        angle = start;
-        transform.RotateAround(target.transform.position, Vector3.forward, angle);
+        damage = 5;
+        weaponCooldowns = 1.5f;
     }
-
 
     void Update()
     {
-        // Spin the object around the target at 20 degrees/second.
-        if (angle >= finish && timer < 0)
-        {
-            dir = -1;
-            stop = true;
-            timer = 100;
-            checkDir();
-            angle = finish;
-            transform.position = new Vector3(target.transform.position.x + 1.5f, target.transform.position.y, 0);
-            transform.rotation = Quaternion.identity;
-            transform.RotateAround(target.transform.position, Vector3.forward, finish);
-            //transform.RotateAround(target.transform.position, Vector3.forward, 180);
-            //transform.RotateAround(target.transform.position, Vector3.forward, 225);
-            //transform.RotateAround(target.transform.position, Vector3.forward, angle);
+        if(!stop){
+            hit();
+            timer = weaponCooldowns; 
+            stop = true;   
         }
-        if (angle <= start && timer < 0)
-        {
-            dir = +1;
-            if(first){
-                first = false;
-            }else{
-                stop = true;
-                timer = 100;
-                checkDir();
-                angle = start;
-                transform.position = new Vector3(target.transform.position.x + 1.5f, target.transform.position.y, 0);
-                transform.rotation = Quaternion.identity;
-                transform.RotateAround(target.transform.position, Vector3.forward, start);
-            }
-        }
-        timer -= Time.deltaTime * 20;
         if(timer < 0){
-            angle += 20 * Time.deltaTime * dir;
-            transform.RotateAround(target.transform.position, Vector3.forward, 20 * Time.deltaTime * dir);
+            stop = false;
         }
+        timer -= Time.deltaTime;
     }
 
-    void checkDir(){
-        switch (direction)
+    void hit(){
+        switch (target.gameObject.GetComponent<PlayerController>().Direction)
         {   
+            case Direction.Right:
+                            this.gameObject.transform.GetChild(0).gameObject.GetComponent<Hitbox>().hit(damage);
+                            this.gameObject.transform.GetChild(4).gameObject.GetComponent<Animation>().Play("HitBoxRight");
+                            break;
             case Direction.Up:
-                            start = 45;
-                            finish = 135;
+                            this.gameObject.transform.GetChild(1).gameObject.GetComponent<Hitbox>().hit(damage);
+                            this.gameObject.transform.GetChild(4).gameObject.GetComponent<Animation>().Play("HitBoxUp");
                             break; 
             case Direction.Left:
-                            start = 135;
-                            finish = 225;
+                            this.gameObject.transform.GetChild(2).gameObject.GetComponent<Hitbox>().hit(damage);
+                            this.gameObject.transform.GetChild(4).gameObject.GetComponent<Animation>().Play("HitBoxLeft");
                             break;
             case Direction.Down:
-                            start = 225;
-                            finish = 315;
+                            this.gameObject.transform.GetChild(3).gameObject.GetComponent<Hitbox>().hit(damage);
+                            this.gameObject.transform.GetChild(4).gameObject.GetComponent<Animation>().Play("HitBoxDown");
                             break;
-            case Direction.Right:
-                            start = 315;
-                            finish = 405;
-                            break;
-
-            default: start = 45;
-                            finish = 135;
+            default: 
                             break;
         }
     }
-
 
 }
