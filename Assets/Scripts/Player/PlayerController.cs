94c97dbc8e16da8d0d0b40845d6f843ee1af9456
Marian <cilteamarian@gmail.com>
2022-09-30 19:38:34 +0300

    added PlayerController base

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,22 @@ namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
-        
+        public float DamageModifier { get; set; }
+        public float SpeedModifier { get; set; }
+
+        public void TakeDamage(int amount)
+        {
+            Debug.Log($"Took damage: {amount}");
+        }
+
+        public void Heal(int amount)
+        {
+            Debug.Log($"Heal: {amount}");
+        }
+
+        public void IncreaseHealth(int amount)
+        {
+            Debug.Log($"Increase health by: {amount}");
+        }
     }
 }
