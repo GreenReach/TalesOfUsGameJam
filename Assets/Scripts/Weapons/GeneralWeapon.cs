4cbee3bb47cc24bd5ed492cb390be46b57ceab54
Ozana <ozana.ionescu@yahoo.ro>
2022-09-30 20:33:01 +0300

    added general sword

---
+++ Assets/Scripts/Weapons/GeneralWeapon.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneralWeapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int damage;
+    public float cooldown;
+    public float weaponCooldowns;
+    public int weaponlvl;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Weapons/Sword.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : GeneralWeapon
+{
+    public GameObject target;
+    public float start =45;
+    public float finish=135;
+    int dir = 0;
+    float angle;
+    void Start()
+    {
+        angle = start;
+        transform.RotateAround(target.transform.position, Vector3.forward, angle);
+    }
+
+
+    void Update()
+    {
+        // Spin the object around the target at 20 degrees/second.
+        angle += 20 * Time.deltaTime * dir;
+        if (angle >= finish)
+        {
+            dir = -1;
+        }
+        if (angle < start)
+        {
+            dir = +1;
+        }
+        transform.RotateAround(target.transform.position, Vector3.forward, angle);
+    }
+
+}
