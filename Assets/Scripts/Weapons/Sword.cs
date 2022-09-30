dc385f7b7d29fc1e0e356f75412e47de98606c64
Ugiababecicu <raresgabrielalexe@yahoo.ro>
2022-09-30 21:07:32 +0300

    Palos

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -7,7 +7,7 @@ public class Sword : GeneralWeapon
     public GameObject target;
     public float start =45;
     public float finish=135;
-    int dir = 0;
+    public int dir = 0;
     float angle;
     void Start()
     {
@@ -23,12 +23,13 @@ public class Sword : GeneralWeapon
         if (angle >= finish)
         {
             dir = -1;
+            Debug.Log(transform.rotation.z);
         }
-        if (angle < start)
+        if (angle <= start)
         {
             dir = +1;
         }
-        transform.RotateAround(target.transform.position, Vector3.forward, angle);
+        transform.RotateAround(target.transform.position, Vector3.forward, 20 * Time.deltaTime * dir);
     }
 
 }
